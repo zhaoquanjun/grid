98831211249744ee995ec322147de4563c04f8c5
Eden <zhaoquanjun0@qq.com>
2019-05-10 11:56:58 +0800

    init respository

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import './grid-ui/global.base.scss'
+import './grid-ui/grid.base.scss'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/views/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>这是首页</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  div {
+    margin: 50px auto 0;
+    font-size: 30px;
+    color: green;
+  }
+</style>
